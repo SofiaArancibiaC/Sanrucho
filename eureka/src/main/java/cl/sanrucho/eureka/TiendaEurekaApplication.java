34c0cc192842be3054cfbfb55632a60477274e3e
Sofia Arancibia <so.arancibiac@duocuc.cl>
2026-08-22 15:14:14 -0400

    Cambios para common

--- eureka/src/main/java/cl/sanrucho/eureka/TiendaEurekaApplication.java
+++ eureka/src/main/java/cl/sanrucho/eureka/TiendaEurekaApplication.java
@@ -2,8 +2,10 @@ package cl.sanrucho.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TiendaEurekaApplication {
 
 	public static void main(String[] args) {
---
+++ ms-common/src/main/java/cl/sanrucho/common/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package cl.sanrucho.common.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String entity, String key, Object value) {
+        super(String.format("No se encontraron registros en %s con el %s '%s'", 
+            entity, key, (value != null ? value.toString() : "N/A")));
+    }
+
+}
+
