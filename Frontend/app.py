c7c83c65d7df1da602b581a5cb03eca45ab256e2
Sofia Arancibia <so.arancibiac@duocuc.cl>
2026-09-01 20:03:00 -0400

    Avance en contacto.html
    + estilos css de contacto.html

--- Frontend/app.py
+++ Frontend/app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template("/publico/index.html")
+    return render_template("/index.html")
 
 @app.route("/base")
 def ficha():
-    return render_template("/publico/base.html")
+    return render_template("/base.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("/contacto.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
