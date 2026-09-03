a5a5236db35792926c953954d74d980c9d1e6273
Camila <cam.cerdad@duocuc.cl>
2026-09-03 13:56:01 -0400

    parte 2

--- Frontend/app.py
+++ Frontend/app.py
@@ -34,5 +34,68 @@ def admin_usuario_form():
 def admin_usuarios():
     return render_template("_admin-usuarios.html")
 
+
+#-------------------------------------------------------------------------------profe 
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+
+@app.route("/producto-detalle")
+def producto_detalle():
+    return render_template("producto-detalle.html")
+
+
+@app.route("/carrito")
+def carrito():
+    return render_template("carrito.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+
+@app.route("/nosotros")
+def nosotros():
+    return render_template("nosotros.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/blog/detalle-1")
+def blog_detalle_1():
+    return render_template("blog-detalle-1.html")
+
+
+@app.route("/blog/detalle-2")
+def blog_detalle_2():
+    return render_template("blog-detalle-2.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
