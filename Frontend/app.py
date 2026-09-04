1c8651e3977252316531b5310292811bc1b2a098
Sofia Arancibia <so.arancibiac@duocuc.cl>
2026-09-04 16:55:54 -0400

    Termina index y login

--- Frontend/app.py
+++ Frontend/app.py
@@ -14,5 +14,13 @@ def ficha():
 def contacto():
     return render_template("/contacto.html")
 
+@app.route("/registro")
+def registro():
+    return render_template("/registro.html")
+
+@app.route("/login")
+def login():
+    return render_template("/login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Frontend/static/js/validaciones.js
@@ -0,0 +1,18 @@
+//----------------------Validación para inicio de sesión en index--------------------------//
+(function () {
+        'use strict'
+        // Obtener todos los formularios a los que queremos aplicar estilos de validación
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Bucle sobre ellos y evitar el envío si no son válidos
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+                }, false)
+            })
+        })()
